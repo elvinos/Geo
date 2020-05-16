77ff2b29b36b4bbf2f9377d8b5c3b3df3bcd7ce7
elvinos <aep.charles@gmail.com>
2020-05-16 20:43:06 +0100

    Fixes to package dependencies

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ APP_NAME=geo
 
 PULL="git pull"
 CHECKOUT="git checkout master"
-BUILD="sudo docker-compose -f docker-compose-prod.yml build"
-PUSH="sudo docker-compose -f docker-compose-prod.yml push"
-STACK_DEPLOY="sudo docker stack deploy -c docker-compose-prod.yml $APP_NAME"
+BUILD="docker-compose -f docker-compose-prod.yml build"
+PUSH="docker-compose -f docker-compose-prod.yml push"
+STACK_DEPLOY="docker stack deploy -c docker-compose-prod.yml $APP_NAME"
 
 #ssh -i ./deploy_key $USER@$SERVER_IP_ADDRESS pwd
 
