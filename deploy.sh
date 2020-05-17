96ad7dd7716ce9aac0621ecb44ec86d0060bb2dc
elvinos <aep.charles@gmail.com>
2020-05-17 16:46:34 +0100

    Fixed Typo #15

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ APP_NAME=geo
 PULL="git pull"
 CHECKOUT="git checkout master"
 PRUNE="docker system prune --volumes -f"
-GET_IMAGE="docker pull $DOCKER_USER/geo:geo-nginx"
-TAG_IMAGE="docker tag $DOCKER_USER/geo:geo-nginx 0.0.0.0:5000/geo-nginx"
+#GET_IMAGE="docker pull $DOCKER_USER/geo:geo-nginx"
+#TAG_IMAGE="docker tag $DOCKER_USER/geo:geo-nginx 0.0.0.0:5000/geo-nginx"
+#TAG_IMAGE="docker tag $DOCKER_USER/geo:geo-backend 0.0.0.0:5000/geo-backend"
 BUILD="docker-compose -f docker-compose-server.yml build"
-PUSH="docker-compose -f docker-compose-server.yml push"
 STACK_DEPLOY="docker stack deploy -c docker-compose-server.yml $APP_NAME"
 
 #ssh -i ./deploy_key $USER@$SERVER_IP_ADDRESS pwd
@@ -23,10 +23,7 @@ ssh -i ./deploy_key -t -t $USER@$SERVER_IP_ADDRESS << EOF
   $PULL
   $CHECKOUT
   $PRUNE
-  $GET_IMAGE
-  $TAG_IMAGE
   $BUILD
-  $PUSH
   $STACK_DEPLOY
   exit
 EOF
